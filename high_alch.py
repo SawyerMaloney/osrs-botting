0b59ac258cc500e6d21a8210b2db2c8be64c4049
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-09-21 18:42:17 -0400

    logout feature

--- high_alch.py
+++ high_alch.py
@@ -20,7 +20,13 @@ class HighAlch:
         self.high_alch_template = cv2.imread("high_alch.png", cv2.IMREAD_UNCHANGED)
         self.high_alch_template = cv2.cvtColor(self.high_alch_template, cv2.COLOR_BGRA2BGR)
 
-        self.high_alch_loc = (0, 0, 0, 0)
+        self.log_out_template = cv2.imread("log_out.png", cv2.IMREAD_UNCHANGED)
+        self.log_out_template = cv2.cvtColor(self.log_out_template, cv2.COLOR_BGRA2BGR)
+
+        self.log_out_button_template = cv2.imread("log_out_button.png", cv2.IMREAD_UNCHANGED)
+        self.log_out_button_template = cv2.cvtColor(self.log_out_button_template, cv2.COLOR_BGRA2BGR)
+
+        self.cached_region = None
 
     def left_click(self):
         self.mouse_ctrl.click(mouse.Button.left, 1)
@@ -51,9 +57,9 @@ class HighAlch:
                 for item in range(len(self.item_locs)):
                     for iter in range(self.stack_sizes[item]):
                         # block until we see the high alch icon
-                        img = self.go_to_image(sct, self.high_alch_template)
+                        img = self.go_to_image(sct, self.high_alch_template, True)
                         while not img:
-                            img = self.go_to_image(sct, self.high_alch_template)
+                            img = self.go_to_image(sct, self.high_alch_template, True)
                         print(f"moving mouse to {self.item_locs[item]}")
                         self.move_mouse(self.item_locs[item])
                         self.wait(default_wait)
@@ -67,16 +73,16 @@ class HighAlch:
             return False
         return True
 
-    def go_to_image(self, sct, image, threshold=.8):
-        if self.high_alch_loc == (0, 0, 0, 0):
+    def go_to_image(self, sct, image, caching, threshold=.8, debug=False):
+        if not caching or self.cached_region == None:
             monitor = sct.monitors[0]
             screenshot = np.array(sct.grab(monitor))
         else:
             region = {
-                "left": self.high_alch_loc[0], 
-                "top": self.high_alch_loc[1], 
-                "height": self.high_alch_loc[2] + 50, 
-                "width": self.high_alch_loc[3] + 50
+                "left": self.cached_region["left"], 
+                "top": self.cached_region["top"], 
+                "height": self.cached_region["height"] + 50, 
+                "width": self.cached_region["width"] + 50
                 }
             screenshot = np.array(sct.grab(region))
         screenshot_gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
@@ -86,25 +92,60 @@ class HighAlch:
         result = cv2.matchTemplate(screenshot_gray, template_gray, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
+        if debug:
+            # Draw rectangle around best match
+            top_left = max_loc
+            h, w = template_gray.shape[:2]
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+
+            # Make a color copy for visualization
+            screenshot_vis = cv2.cvtColor(screenshot_gray, cv2.COLOR_GRAY2BGR)
+            cv2.rectangle(screenshot_vis, top_left, bottom_right, (0, 0, 255), 2)
+
+            # Show the image
+            cv2.imshow("Template Match", screenshot_vis)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
         if max_val > threshold:
             t_height, t_width = template_gray.shape[:2]
             center_x = max_loc[0] + t_width // 2
             center_y = max_loc[1] + t_height // 2
-            if self.high_alch_loc == (0, 0, 0, 0):
-                self.high_alch_loc = (max_loc[0], max_loc[1], t_height, t_width)
-            else:
+            if caching and self.cached_region is not None:
                 # convert to global screenspace
-                center_x += self.high_alch_loc[0]
-                center_y += self.high_alch_loc[1]
+                center_x += self.cached_region["left"]
+                center_y += self.cached_region["top"]
+            elif caching:
+                self.cached_region = {
+                "left": max_loc[0],
+                "top": max_loc[1],
+                "height": t_height,
+                "width": t_width
+                }
+
             print(f"moving to {center_x}, {center_y} and clicking")
             self.move_mouse((center_x, center_y))
             self.wait()
             self.left_click()
             self.wait
-            
+           
             return True
         else:
+            print(f"No good match. max_val: {max_val}")
             return False # no good match
+        
+    def log_out(self):
+        with mss.mss() as sct:
+            print("Going to logout screen...")
+            self.go_to_image(sct, self.log_out_template, False)
+            self.wait()
+            print("Pressing log out...")
+            img = self.go_to_image(sct, self.log_out_button_template, False, threshold=.5)  # bye bye!
+            while not img:
+                img = self.go_to_image(sct, self.log_out_button_template, False, threshold=.5)  # bye bye!
+            print("Log out pressed.")
+            return True
+
 
     def run(self):
         num_items = int(input("how many items: "))
@@ -123,6 +164,8 @@ class HighAlch:
             listener.join()
 
         print(f"High alching finished at {datetime.fromtimestamp(time.time())}.")
+
+        self.log_out()
 
 if __name__ == "__main__":
     bot = HighAlch()
